40416ed913326a9ddf0339b80b783636ac9b687b
shathishwarma <shathishwarma@gmail.com>
2023-06-04 06:28:33 +0530

    Changes is app.js and user.js

--- app.js
+++ app.js
@@ -1,72 +1,76 @@
+// Import required packages
+const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const User = require('./models/user'); // Import the User model
 
-mongoose.connect('mongodb://localhost/myapp', { useNewUrlParser: true, useUnifiedTopology: true })
+// Create an Express application
+const app = express();
+
+// Set up middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Connect to MongoDB Atlas
+mongoose.connect('mongodb+srv://shathishwarma:B6YJ9siF1KRPMmTT@ecommerce.vt6lhcu.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB Atlas');
   })
   .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
+    console.error('Error connecting to MongoDB Atlas:', err);
   });
-  const express = require('express');
-  const mongoose = require('mongoose');
-  const bcrypt = require('bcrypt');
-  const app = express();
-  
-  app.set('view engine', 'ejs');
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static('public'));
-  
-  // Add the MongoDB connection code here
-  
-  const User = require('./models/user');
-  app.get('/signup', (req, res) => {
-    res.render('signup');
-  });
-  
-  app.post('/signup', async (req, res) => {
-    const { username, password } = req.body;
-  
+
+// Define the User model
+const User = mongoose.model('User', new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+}));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+app.post('/signup', async (req, res) => {
     try {
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
       const user = new User({
-        username: username,
-        password: hashedPassword
+        username: req.body.username,
+        password: req.body.password
       });
-  
       await user.save();
-  
       res.redirect('/login');
-    } catch (error) {
-      console.error('Error creating user:', error);
+    } catch (err) {
+      console.error('Error signing up:', err);
       res.redirect('/signup');
     }
   });
-  
-  app.get('/login', (req, res) => {
-    res.render('login');
-  });
-  
-  app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-  
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', async (req, res) => {
     try {
-      const user = await User.findOne({ username: username });
-  
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-  
-      if (isPasswordValid) {
-        res.send('Login successful!');
-      } else {
-        res.send('Invalid username or password');
+      const user = await User.findOne({ username: req.body.username });
+      if (!user) {
+        throw new Error('Invalid username or password');
       }
-    } catch (error) {
-      console.error('Error logging in:', error);
-      res.send('An error occurred');
+      const isPasswordValid = await user.comparePassword(req.body.password);
+      if (!isPasswordValid) {
+        throw new Error('Invalid username or password');
+      }
+      res.redirect('/dashboard');
+    } catch (err) {
+      console.error('Error logging in:', err);
+      res.redirect('/login');
     }
   });
-  const port = 3000;
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-  
+// Start the server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
--- models/user.js
+++ models/user.js
@@ -1,8 +1,37 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
+// Define the User schema
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 });
 
-module.exports = mongoose.model('User', userSchema);
+// Hash the password before saving the user
+userSchema.pre('save', async function(next) {
+  try {
+    const user = this;
+    if (!user.isModified('password')) {
+      return next();
+    }
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+    next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// Compare the provided password with the hashed password
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+// Create the User model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
