162c2be93d60033a82523eed09c3d007275d3293
shathishwarma <shathishwarma@gmail.com>
2023-06-04 06:34:08 +0530

    Code changes in app.js

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // Connect to MongoDB Atlas
-mongoose.connect('mongodb+srv://shathishwarma:B6YJ9siF1KRPMmTT@ecommerce.vt6lhcu.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect('mongodb+srv://shathishwarma:B6YJ9siF1KRPMmTT@ecommerce.vt6lhcu.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB Atlas');
   })
@@ -22,10 +23,20 @@ mongoose.connect('mongodb+srv://shathishwarma:B6YJ9siF1KRPMmTT@ecommerce.vt6lhcu
   });
 
 // Define the User model
-const User = mongoose.model('User', new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true }
-}));
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+const User = mongoose.model('User', userSchema);
 
 // Routes
 app.get('/', (req, res) => {
@@ -37,39 +48,40 @@ app.get('/signup', (req, res) => {
 });
 
 app.post('/signup', async (req, res) => {
-    try {
-      const user = new User({
-        username: req.body.username,
-        password: req.body.password
-      });
-      await user.save();
-      res.redirect('/login');
-    } catch (err) {
-      console.error('Error signing up:', err);
-      res.redirect('/signup');
-    }
-  });
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+    await user.save();
+    res.redirect('/login');
+  } catch (err) {
+    console.error('Error signing up:', err);
+    res.redirect('/signup');
+  }
+});
 
 app.get('/login', (req, res) => {
   res.render('login');
 });
 
 app.post('/login', async (req, res) => {
-    try {
-      const user = await User.findOne({ username: req.body.username });
-      if (!user) {
-        throw new Error('Invalid username or password');
-      }
-      const isPasswordValid = await user.comparePassword(req.body.password);
-      if (!isPasswordValid) {
-        throw new Error('Invalid username or password');
-      }
-      res.redirect('/dashboard');
-    } catch (err) {
-      console.error('Error logging in:', err);
-      res.redirect('/login');
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      throw new Error('Invalid username or password');
     }
-  });
+    const isPasswordValid = await user.comparePassword(req.body.password);
+    if (!isPasswordValid) {
+      throw new Error('Invalid username or password');
+    }
+    res.redirect('/dashboard');
+  } catch (err) {
+    console.error('Error logging in:', err);
+    res.redirect('/login');
+  }
+});
+
 // Start the server
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
